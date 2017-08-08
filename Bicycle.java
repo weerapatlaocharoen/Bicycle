eff8b7d7453761f47c075bad2562197d9fa86f50
weerapatlaocharoen <weerapatonline@gmail.com>
2017-08-08 10:49:06 +0700

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -18,6 +18,6 @@ class Bicycle {
 		}
 
 		void printStates(){
-		System.out.println("cadence:"+ cadence + "speed" + speed + "gear" + gear);
+		System.out.println("cadence"+ cadence + "speed" + speed + "gear" + gear);
 	}
 }
