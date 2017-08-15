aa05ffe031d294df176727fbc4688cd409d9f44d
weerapatDEKIT <weerapatonline@gmail.com>
2017-08-15 09:46:33 +0700

    interface Bicycle

--- 3wheelBicycle.java
+++ 3wheelBicycle.java
@@ -1,6 +1,7 @@
 class ThreeWheelBicycle extends Bicycle{
 	int wheel = 3;
   void printStates(){
+  	super.printStates();
   System.out.println("cadence=> " + cadence + " speed=> " + speed + " gear=> " + gear + " wheel=> " + wheel);
 
 }
--- Bicycle.java
+++ Bicycle.java
@@ -1,24 +1,10 @@
-class Bicycle {
-   int cadence = 0;
-   int speed = 1;
-   int gear = 1;
+interface Bicycle{
 
-  void changecCadence(int newValue){
-    cadence = newValue;
-  }
-   void changeGear(int newValue){
-    gear = newValue;
-   }
-
-  void speedUp(int increment){
-   speed = speed + increment;
-  }
-
- void applyBreak(int decrement){
-   speed = speed - decrement;
-  }
-
-  void printStates(){
-  System.out.println("cadence=>" + cadence + " speed=>" + speed + " gear=>" + gear);
- }
+   void changecCadence(int newValue)
+   
+   void changeGear(int newValue)
+   
+   void speedUp(int increment)
+   
+   void applyBreak(int decrement) 
 }
