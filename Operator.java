f17cf391da74e09db2e28f12b7dba08d51b109d3
weerapatDEKIT <weerapatonline@gmail.com>
2017-09-05 11:14:21 +0700

    5 กันยายน 2560 Oparator

---
+++ Operator.java
@@ -0,0 +1,38 @@
+class Operator {
+ public static void main(String Arg[]){
+   /* int x = 7;
+    boolean b = x <= 7;
+      System.out.println(b);*/
+   
+   /*String x = "weerapat ";
+   String y = "laocharoen";
+   boolean z = x != y;
+   System.out.println(z);*/
+   
+  /* int x = 8;
+   int y = 7;
+   if (x != y)
+   System.out.println(x+y);
+   System.out.println(x+y);*/
+   
+  /* int x = 1;
+   int y = 2;
+   if((x == 1)&&(y == 2));
+   System.out.println("x is 1 AND y is 2");
+   if((x == 1)||(y == 1));
+   System.out.println("x is 1 or y is 1");*/
+   
+   /* int x = 1;
+   int y = 2;
+   int z;
+   boolean vvv = false;
+   z = vvv ? x : y;
+   System.out.println(z); */
+   
+   Parent obj1 = new Parent();
+   Parent obj2 = new Child();
+   
+   System.out.println(obj1 instrant)
+     
+}
+}
