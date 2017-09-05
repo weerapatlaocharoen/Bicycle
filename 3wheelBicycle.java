48bd7e2fa0ea811989db881502eae05aa3889536
weerapatlaocharoen <weerapatonline@gmail.com>
2017-09-05 11:28:49 +0700

    Delete 3wheelBicycle.java

--- 3wheelBicycle.java
+++
@@ -1,8 +0,0 @@
-class ThreeWheelBicycle extends Bicycle{
-	int wheel = 3;
-  void printStates(){
-  	super.printStates();
-  System.out.println("cadence=> " + cadence + " speed=> " + speed + " gear=> " + gear + " wheel=> " + wheel);
-
-}
-}
